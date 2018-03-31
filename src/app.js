339f1b8393d7118790262ebcd76ac2ff68b16232
Rafael da S.P. de Azevedo <rafaelsilva.fatec@gmail.com>
2018-03-31 19:35:34 -0300

    Separação em componentes

--- src/app.js
+++ src/app.js
@@ -1,46 +1,35 @@
 'use strict'
 
 import React, { Component } from 'react'
+import Search from './components/search'
+import UserInfo from './components/user-info'
+import Actions from './components/actions'
+import Repos from './components/repos'
 
 const App = () => (
   <div className='app'>
+    <Search />
+    <UserInfo />
+    <Actions />
 
-    <div className='search'>
-      <input type='search' placeholder='Digite o nome do usuário no GitHub'/>
-    </div>
+    <Repos
+      className='repos'
+      title='Repositórios:'
+      repos={[{
+        name: 'Nome do repositório',
+        link: '#'
+      }]}
+    />
 
-    <div className='user-info'>
-      <img src='https://avatars2.githubusercontent.com/u/15950775?v=4' />
-      <h1>
-        <a href='https://github.com/azevedorafael' >Rafael Azevedo</a>
-      </h1>
+    <Repos
+      className='starred'
+      title='Favoritos:'
+      repos={[{
+        name: 'Nome do repositório',
+        link: '#'
+      }]}
+    />
 
-      <ul className='repos-info'>
-        <li>- Repositórios: 122</li>
-        <li>- Seguidores: 19</li>
-        <li>- Seguindo: 10</li>
-      </ul>
-
-      <div className='actions'>
-        <button>Ver repositórios</button>
-        <button>Ver favoritos</button>
-      </div>
-
-      <div className='repos'>
-        <h2>Repositórios:</h2>
-        <ul>
-          <li><a href='#'>Nome do repositório</a></li>
-        </ul>
-      </div>
-
-      <div className='starred'>
-        <h2>Favoritos:</h2>
-        <ul>
-          <li><a href='#'>Nome do repositório</a></li>
-        </ul>
-      </div>
-
-    </div>
   </div>
 )
 
---
+++ src/components/user-info.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const UserInfo = () => (
+    <div className='user-info'>
+        <img src='https://avatars2.githubusercontent.com/u/15950775?v=4' />
+        <h1>
+            <a href='https://github.com/azevedorafael' >Rafael Azevedo</a>
+        </h1>
+
+        <ul className='repos-info'>
+            <li>- Repositórios: 122</li>
+            <li>- Seguidores: 19</li>
+            <li>- Seguindo: 10</li>
+        </ul>
+    </div>
+)
+
+export default UserInfo
